85de009b094e4858098d94842fc61980475d9db0
Syed Shahzer <syedshahzer@Syeds-MacBook-Air.local>
2025-05-11 00:35:00 +0530

    change gemini model

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -3,7 +3,7 @@ from google.adk.agents import Agent
 root_agent = Agent(
     name="greeting_agent",
     # https://ai.google.dev/gemini-api/docs/models
-    model="gemini-2.0-flash",
+    model="gemini-2.5-flash-preview-04-17",
     description="Greeting agent",
     instruction="""
     You are a helpful assistant that greets the user. 
